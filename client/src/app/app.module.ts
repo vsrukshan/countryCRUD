2da89893f3ccab131d4908aca910b15234ad78ed
vsrukshan <vsrukshan@gmail.com>
2020-12-19 14:53:26 +0530

    initial commint

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialComponentModule} from './material-component/material-component.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import { CountryFormComponent } from './views/country/country-form/country-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountryFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/views/country/country-form/country-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-country-form',
+  templateUrl: './country-form.component.html',
+  styleUrls: ['./country-form.component.scss']
+})
+export class CountryFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
