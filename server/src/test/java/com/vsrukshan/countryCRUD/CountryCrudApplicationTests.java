39af06389ab5dbb47c54903b0818aed665f439be
vsrukshan <vsrukshan@gmail.com>
2020-12-19 10:22:02 +0530

    initial commit

---
+++ server/src/test/java/com/vsrukshan/countryCRUD/CountryCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsrukshan.countryCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
